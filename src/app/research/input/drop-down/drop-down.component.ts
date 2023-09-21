90900a0bb79592fb6f0323e3f4febba95b668209
Izu2001 <thanhtrungtran13@gmail.com>
2023-09-21 10:50:03 +0700

    feat: fix dropdown icon error

--- src/app/research/input/drop-down/drop-down.component.ts
+++ src/app/research/input/drop-down/drop-down.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSelect } from '@angular/material/select';
 
 @Component({
   selector: 'app-drop-down',
   templateUrl: './drop-down.component.html',
-  styleUrls: ['./drop-down.component.scss']
+  styleUrls: ['./drop-down.component.scss'],
 })
 export class DropDownComponent {
+  @ViewChild('selector', { static: false }) selector?: MatSelect;
 
+  onSelectionChange(value: any) {
+    this.selector?.close();
+  }
 }
